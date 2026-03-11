626e970a89aa6da23cd7b71a6acd9dae44bbd3eb
knj <rlaskawns888@gmail.com>
2026-03-11 23:42:55 +0900

    init: spring boot like server project

---
+++ src/main/java/com/springstudy/likeserver/LikeserverApplication.java
@@ -0,0 +1,13 @@
+package com.springstudy.likeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springstudy/likeserver/LikeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springstudy.likeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
